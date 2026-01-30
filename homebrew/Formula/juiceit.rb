92348e2823a0060165a59c85ef2e8809102345c0
Brian Slate <brian.slate@caylent.com>
2026-01-30 13:20:42 -0800

    Add Homebrew installation support
    - Create bin/juiceit executable wrapper for global installation
    - Add bin entry to package.json
    - Update Homebrew formula with npm install and proper dependencies
    - Add ffmpeg dependency for ffprobe
    - Point to bin/juiceit instead of juiceit.js
    - Ready for brew install juiceit

--- homebrew/Formula/juiceit.rb
+++ homebrew/Formula/juiceit.rb
@@ -1,22 +1,27 @@
 class Juiceit < Formula
-    desc "A script to rip DVDs using HandBrakeCLI"
-    homepage "https://github.com/brian-slate/juice-it"
-    url "https://github.com/brian-slate/juice-it/releases/download/v1.0.11/juice-it-v1.0.11.tar.gz"
-    sha256 "0cbf1ce078f126894b7ec3d254ba644de0dd3a1a915f97adb48c011eeaaedcde"
+  desc "Smart DVD ripper with automatic metadata lookup and episode naming"
+  homepage "https://github.com/brian-slate/juice-it"
+  url "https://github.com/brian-slate/juice-it/releases/download/v1.0.11/juice-it-v1.0.11.tar.gz"
+  sha256 "0cbf1ce078f126894b7ec3d254ba644de0dd3a1a915f97adb48c011eeaaedcde"
+  license "MIT"
 
-    depends_on "node"
-    depends_on "handbrake"
-    depends_on "libdvdcss"
+  depends_on "node"
+  depends_on "handbrake"
+  depends_on "libdvdcss"
+  depends_on "ffmpeg"  # For ffprobe to check video durations
 
-    def install
-      # Install the entire package
-      libexec.install Dir["*"]  # This installs everything in the current directory to libexec
-
-      # Symlink the juiceit executable to the bin directory
-      bin.install_symlink libexec/"juiceit.js" => "juiceit"
-    end
-
-    test do
-      system "#{bin}/juiceit", "--help"
-    end
+  def install
+    # Install npm dependencies
+    system "npm", "install", "--production", "--ignore-scripts"
+    
+    # Install all files to libexec
+    libexec.install Dir["*"]
+    
+    # Symlink the executable
+    bin.install_symlink libexec/"bin/juiceit"
   end
+
+  test do
+    system "#{bin}/juiceit", "--help"
+  end
+end
